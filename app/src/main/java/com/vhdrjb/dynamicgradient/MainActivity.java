11b43d9f6202e3d53c2dd36323077c174376cd0b
Vahid Rajabi <panda.soft.group@gmail.com>
2018-07-08 12:50:11 +0430

    Dynamic Gradient

--- app/src/main/java/com/vhdrjb/dynamicgradient/MainActivity.java
+++ app/src/main/java/com/vhdrjb/dynamicgradient/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 
+import ir.vhdrjbs.dgradient.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
